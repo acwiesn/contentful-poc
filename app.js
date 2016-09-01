d795f31646e23d341d2614d4fe16bea9f1c01dd0
acwiesn <adam.wiesner@tcs.com>
2016-09-01 11:09:17 -0400

    push changes to github

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index.js');
 var users = require('./routes/users');
 
 var app = express();
+
+var contentful = require('contentful')
+var client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: 'First Project',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: '5a3f12262a5b6da43ee8b8dd3e4d24a299122cc58fe164b4b0f58ad351e30602'
+})
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+client.getEntry('78rg6aNOgHtPXbfnPeFyad')
+.then((entry) => console.log(entry)).catch(function (error){
+  console.log(error)
+}
+);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+/* var contentful = require('contentful')
+
+var client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: 'First Project',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: '5a3f12262a5b6da43ee8b8dd3e4d24a299122cc58fe164b4b0f58ad351e30602'
+})
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+client.getEntry('78rg6aNOgHtPXbfnPeFyad')
+.then((entry) => console.log(entry))
+*/
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
